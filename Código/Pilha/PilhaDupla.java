845806c9874926609ff7846097dfbc19b48130d8
Vinícius <viniciusandradebc@gmail.com>
2025-03-24 09:47:29 -0300

    Adicionando a Pilha Dupla

---
+++ Código/Pilha/PilhaDupla.java
@@ -0,0 +1,161 @@
+public class PilhaDupla implements EmpilhavelDupla {
+    public Object[] dados;
+    public int ponteiroTopoInverso;
+    public int ponteiroTopo;
+
+    public PilhaDupla(){
+        this(10);
+    }
+
+    public PilhaDupla(int tamanho) {
+        this.dados = new Object[tamanho];
+        this.ponteiroTopoInverso = dados.length;
+        this.ponteiroTopo = -1;
+    }
+    //Métodos necessários 
+    @Override
+    public void empilhar1(Object dado){
+        if(!this.estaCheia1()) {
+            ponteiroTopo++;
+            dados[ponteiroTopo] = dado;
+        } else {
+            System.err.println("Stack is full!");
+        }
+    }
+
+    @Override
+    public Object desempilhar1(){
+        Object aux = null;
+        if(!this.estaVazia1()) {
+            aux = dados[ponteiroTopo];
+            ponteiroTopo--;
+        } else {
+            System.err.println("Stack is empty");
+        }
+
+        return aux;
+    }
+    @Override
+    public void atualizar1(Object dado) {
+        if(!this.estaVazia1()) {
+            dados[ponteiroTopo] = dado;
+        } else {
+            System.err.println("Stack is empty!");
+        }
+    }
+
+    @Override
+    public String imprimir1(){
+        String resultado = "[";
+
+        if(!estaVazia1()) {
+            for(int i = 0; i <= ponteiroTopo; i++) {
+                if(i != ponteiroTopo) {
+                    resultado += dados[i] + ", ";
+                } else {
+                    resultado += dados[i];
+                }
+            }
+        } else {
+            System.err.println("Stack is empty!");
+        }
+
+        return resultado + "]";
+    }
+    //Métodos auxiliares
+    @Override
+    public Object espiar1(){
+        Object aux = null;
+        if(!this.estaVazia1()) {
+            aux = dados[ponteiroTopo];
+        } else {
+            System.err.println("Stack is empty");
+        }
+
+        return aux;
+    }
+
+    @Override
+    public boolean estaCheia1(){
+        return (ponteiroTopo + 1 == ponteiroTopoInverso);
+    }
+
+    @Override
+    public boolean estaVazia1(){
+        return (ponteiroTopo == -1);
+    }
+
+    @Override
+    public void empilhar2(Object dado){
+        if(!this.estaCheia2()) {
+            ponteiroTopoInverso--;
+            dados[ponteiroTopoInverso] = dado;
+        } else {
+            System.err.println("Stack is full!");
+        }
+    }
+
+    @Override
+    public Object desempilhar2(){
+        Object aux = null;
+        if(!this.estaVazia2()) {
+            aux = dados[ponteiroTopoInverso];
+            ponteiroTopoInverso++;
+        } else {
+            System.err.println("Stack is empty");
+        }
+
+        return aux;
+    }
+    @Override
+    public void atualizar2(Object dado) {
+        if(!this.estaVazia2()) {
+            dados[ponteiroTopoInverso] = dado;
+        } else {
+            System.err.println("Stack is empty!");
+        }
+    }
+
+    @Override
+    public String imprimir2(){
+        String resultado = "[";
+
+        if(!estaVazia2()) {
+            for(int i = ponteiroTopoInverso; i < dados.length; i++) {
+                if(i != dados.length-1) {
+                    resultado += dados[i] + ", ";
+                } else {
+                    resultado += dados[i];
+                }
+            }
+        } else {
+            System.err.println("Stack is empty!");
+        }
+
+        return resultado + "]";
+    }
+    //Métodos auxiliares
+    @Override
+    public Object espiar2(){
+        Object aux = null;
+        if(!this.estaVazia2()) {
+            aux = dados[ponteiroTopoInverso];
+        } else {
+            System.err.println("Stack is empty");
+        }
+
+        return aux;
+    }
+
+    @Override
+    public boolean estaCheia2(){
+        return (ponteiroTopoInverso -1 == ponteiroTopo);
+    }
+
+    @Override
+    public boolean estaVazia2(){
+        return (ponteiroTopoInverso == dados.length);
+    }
+
+
+}
--- Código/Pilha/PilhaMain.java
+++ Código/Pilha/PilhaMain.java
@@ -1,6 +1,9 @@
 public class PilhaMain {
     public static void main(String[] args) {
         Empilhavel p = new PilhaEstatica();
+        Empilhavel p2 = new PilhaEstatica();
+        Empilhavel p3 = new PilhaInversa();
+        EmpilhavelDupla p4 = new PilhaDupla(15);
         p.empilhar("O");
         p.empilhar("Astronauta");
         p.empilhar("de");
@@ -9,5 +12,37 @@ public class PilhaMain {
 
         p.desempilhar();
         System.out.println(p.imprimir());
+
+        p2.empilhar("Azul");
+        System.out.println(p2.imprimir());
+
+        p3.empilhar("Mármore");
+        p3.empilhar("de");
+        p3.empilhar("Astronauta");
+        p3.empilhar("O");
+        p3.empilhar("Azul");
+        p3.desempilhar();
+
+        System.out.println(p3.imprimir());
+
+        p4.empilhar1("O");
+        p4.empilhar1("Astronauta");
+        p4.empilhar1("de");
+        p4.empilhar1("Mármore");
+        p4.empilhar1("Azul");
+        p4.desempilhar1();
+
+        p4.empilhar2("Mármore");
+        p4.empilhar2("de");
+        p4.empilhar2("Astronauta");
+        p4.empilhar2("O");
+        p4.empilhar2("Azul");
+        p4.desempilhar2();
+
+        System.out.println(p4.imprimir1());
+        System.out.println(p4.imprimir2());
+
+
+
     }
 }
